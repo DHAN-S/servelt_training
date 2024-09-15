763a34dab9e6bad557b695b6554d4e0e39b58b2c
DHANUSH S <dhanushsubramaniyam26@gmail.com>
2024-09-15 06:27:41 +0530

    add a  session

--- src/main/java/com/dhanush/AddClass.java
+++ src/main/java/com/dhanush/AddClass.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -15,11 +16,12 @@ public class AddClass  extends HttpServlet {
         int num1=Integer.parseInt(req.getParameter("one"));
         int num2=Integer.parseInt(req.getParameter("two"));
         int num3=num1+num2;
-
-
-
+//session
+        HttpSession sess = req.getSession();
+        sess.setAttribute("num3",num3);
+        res.sendRedirect("multiply");
         //send redirect method url method
-        res.sendRedirect("multiply?answer="+num3);
+       // res.sendRedirect("multiply?answer="+num3);
 
 
 //request dispatcher method
--- src/main/java/com/dhanush/MultiplyClass.java
+++ src/main/java/com/dhanush/MultiplyClass.java
@@ -3,15 +3,36 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 public class MultiplyClass extends HttpServlet {
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         PrintWriter ss=res.getWriter();
-        int answer= (Integer.parseInt(req.getParameter("answer")));
-        ss.println(answer);
-        System.out.println("in multiply sendredirect");
+
+        //session
+        HttpSession ses = req.getSession();
+        //ses.removeAttribute("num3");
+        int num3=(int)ses.getAttribute("num3");
+        ss.println(num3);
+
+
+
+
+
+
+
+
+
+
+
+//        int answer= (Integer.parseInt(req.getParameter("answer")));
+//        ss.println(answer);
+//        System.out.println("in multiply sendredirect");
 
 //        int values=(int)(req.getAttribute("mul"));
 //
